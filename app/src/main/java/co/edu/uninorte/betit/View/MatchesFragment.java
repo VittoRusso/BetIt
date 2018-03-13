745986aab890aa8946d2d401fe80a7a41cef118a
gaabgonca <32182942+gaabgonca@users.noreply.github.com>
2018-03-13 14:19:27 -0500

    Was able to implement unique fragment for Matches, to be developed

---
+++ app/src/main/java/co/edu/uninorte/betit/View/MatchesFragment.java
@@ -0,0 +1,92 @@
+package co.edu.uninorte.betit.View;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import co.edu.uninorte.betit.R;
+
+/**
+ * Created by Gabriel on 11/03/2018.
+ */
+
+public class MatchesFragment extends Fragment {
+
+    private static final String PAGE_TITLE = "PAGE_TITLE";
+    private static final String NEW_CONTENT = "NEW_CONTENT";
+
+    private String pageTitle;
+    private MatchesFragmentCallback callback;
+    private String newContent;
+
+    public MatchesFragment(){}
+
+    public static MatchesFragment getInstance(String pageTitle, String newContent){
+                MatchesFragment fragment = new MatchesFragment();
+                Bundle args = new Bundle();
+                args.putString(PAGE_TITLE, pageTitle);
+                args.putString(NEW_CONTENT, newContent);
+                fragment.setArguments(args);
+                return fragment;
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            this.pageTitle = getArguments().getString(PAGE_TITLE);
+            this.newContent = getArguments().getString(NEW_CONTENT);
+            }
+        else {
+            Log.d("TAG", "Well... F***.");
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_view_pager_item, container, false);
+        TextView content = ((TextView) v.findViewById(R.id.lbl_pager_item_content));
+        content.setText(pageTitle);
+        content.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                callback.onPagerItemClick(
+                       // ((TextView) v).getText().toString()
+                        newContent
+                );
+
+
+            }
+        });
+
+        return v;
+    }
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof MatchesFragmentCallback) {
+            callback = (MatchesFragmentCallback) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement FragmentPagerItemCallback");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        callback = null;
+    }
+
+
+    public interface  MatchesFragmentCallback {
+        void onPagerItemClick(String message);
+    }
+}
