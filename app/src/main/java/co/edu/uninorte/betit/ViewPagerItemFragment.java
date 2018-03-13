007e5c35375673bf0b7aca25527a7694a222d298
gaabgonca <32182942+gaabgonca@users.noreply.github.com>
2018-03-13 10:44:52 -0500

    Took back something dumb i did last night

--- app/src/main/java/co/edu/uninorte/betit/ViewPagerItemFragment.java
+++ app/src/main/java/co/edu/uninorte/betit/ViewPagerItemFragment.java
@@ -8,6 +8,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import co.edu.uninorte.betit.Data.Bet;
+import co.edu.uninorte.betit.Data.Match;
+import co.edu.uninorte.betit.Data.User;
 
 /**
  * Created by Gabriel on 11/03/2018.
@@ -16,9 +21,19 @@ import android.widget.TextView;
 public class ViewPagerItemFragment extends Fragment {
 
     private static final String PAGE_TITLE = "PAGE_TITLE";
+    private static final String MATCHES = "MATCHES";
+    private static final String BETS = "BETS";
+    private static final String USER = "USER";
+
+    public static final String[] pageTitles = ViewPagerActivity.pageTitles;
+
 
     private String pageTitle;
     private FragmentPagerItemCallback callback;
+    private Match[] matches;
+    private Bet[] bets;
+    private User[] user;
+    int index;
 
     public ViewPagerItemFragment(){}
 
@@ -30,11 +45,57 @@ public class ViewPagerItemFragment extends Fragment {
         return fragment;
     }
 
+    public static ViewPagerItemFragment getInstance(Match[] matches){
+        ViewPagerItemFragment fragment = new ViewPagerItemFragment();
+        Bundle args = new Bundle();
+        args.putSerializable(MATCHES,matches);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public static ViewPagerItemFragment getInstance(Bet[] bets){
+        ViewPagerItemFragment fragment = new ViewPagerItemFragment();
+        Bundle args = new Bundle();
+        args.putSerializable(BETS,bets);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public static ViewPagerItemFragment getInstance(User user){
+        ViewPagerItemFragment fragment = new ViewPagerItemFragment();
+        Bundle args = new Bundle();
+        args.putSerializable(USER,user);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
             this.pageTitle = getArguments().getString(PAGE_TITLE);
+
+            for (int i=0; i<pageTitles.length;i++){
+                if (pageTitles[index]== pageTitle){
+                    this.index = i;
+                    break;
+                }
+            }
+            switch (index){
+                case 0:
+                    Toast.makeText(getContext(),"Henlo choq 1",Toast.LENGTH_SHORT).show();
+                    break;
+                case 1:
+                    Toast.makeText(getContext(),"Henlo choq 2",Toast.LENGTH_SHORT).show();
+                    break;
+                case 2:
+                    Toast.makeText(getContext(),"Henlo choq 3",Toast.LENGTH_SHORT).show();
+                    break;
+                default:
+                    Toast.makeText(getContext(),"cmamut",Toast.LENGTH_SHORT).show();
+                    break;
+            }
+
         } else {
             Log.d("TAG", "Well... F***.");
         }
