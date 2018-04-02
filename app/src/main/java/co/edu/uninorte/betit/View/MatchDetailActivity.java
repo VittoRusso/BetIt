691955c3dbdb9e4adcec24ca6eb1f06b6a3d09ad
VittoRusso <vitto1996@gmail.com>
2018-04-02 08:42:33 -0500

    Bet statistics added and users leaderboard. Act 2

--- app/src/main/java/co/edu/uninorte/betit/View/MatchDetailActivity.java
+++ app/src/main/java/co/edu/uninorte/betit/View/MatchDetailActivity.java
@@ -344,6 +344,7 @@ public class MatchDetailActivity extends AppCompatActivity {
                         if(win2<0){
                             tieWins++;
                         }
+
                     }
                 }
             }
